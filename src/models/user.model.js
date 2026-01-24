d184d76f91dda30b7330275ada1ffd53e2454476
AsimSiddiqui793264 <asimsiddiqui939@gmail.com>
2026-01-23 19:47:26 -0800

    feat: integrate Cloudinary for file uploads and add multer middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+    {
     username: {
         type: String,
         required: true,
