5765f9739531d010927654d87eed06ce2be1f98b
AsimSiddiqui793264 <asimsiddiqui939@gmail.com>
2026-01-23 20:23:19 -0800

    feat: implement user registration endpoint with async error handling

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (handleRequest) =>{
-    (req , res , next) =>{
+   return (req , res , next) =>{
         Promise.resolve(handleRequest(req , res , next))
         .catch((err) => next(err))
     }
